524ffd7b04aae644e71b2f53acd76ff309ce9f20
Christian Perez <christian.manuel.perez@gmail.com>
2016-03-09 15:40:43 -0800

    remove unused images and update gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 	grunt.registerTask('serve' ,['sass', 'autoprefixer', 'watch']);
 }
