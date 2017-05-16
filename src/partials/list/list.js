6dfbae2484226f4b7380dc1a57ba5b9a586fcbdf
josemeza <jose.meza.ramos@gmail.com>
2017-05-15 23:06:42 -0700

    nudge

--- src/partials/list/list.js
+++ src/partials/list/list.js
@@ -33,7 +33,7 @@ angular.module('app.list', [])
       'organization': 'Self',
       'description': ['ibid.','ibid'],
       'img': 'dist/assets/compressed/Amazon/N3a.png',
-      'width': 'img-1-3 off-1-8',
+      'width': 'img-1-3',
       'url': 'amazon'
     },
     'princeton-mellon': {
