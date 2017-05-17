40566d93200836511312ae6167689eeb49d25bd8
josemeza <jose.meza.ramos@gmail.com>
2017-05-17 00:20:20 -0700

    add to amazon

--- src/partials/list/list.js
+++ src/partials/list/list.js
@@ -31,7 +31,7 @@ angular.module('app.list', [])
       'year': '2017',
       'title': 'Imagery',
       'organization': 'Self-Initiated',
-      'description': ['Lorem ipsum.','Lorem ipsum.'],
+      'description': ['Analog-digital mass-production blah blah.','Rauschenberg blah blah Pictures Generation blah blah Wade/Guyton blah blah.'],
       'img': 'dist/assets/compressed/Amazon/N3a.png',
       'width': 'img-1-4',
       'url': 'amazon'
